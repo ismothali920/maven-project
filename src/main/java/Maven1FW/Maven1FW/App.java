805bd432bee06a3fce55bf491713ff464b61a8fc
ismothali920 <ismothali213@gmail.com>
2020-01-21 17:55:19 -0500

    izzy first commit

---
+++ src/main/java/Maven1FW/Maven1FW/App.java
@@ -0,0 +1,13 @@
+package Maven1FW.Maven1FW;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
